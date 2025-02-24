90d1581677d776c22b8378e66ffbe98222cebc5c
developer111222 <anishprajapati765@gmail.com>
2025-02-24 14:10:39 +0530

    first commit

--- src/utils/ImageUrl.jsx
+++ src/utils/ImageUrl.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const ImageUrl = () => {
     const url =
   // 'http://localhost:5000/upload'
-    'https://goldfish-app-hbefc.ondigitalocean.app//upload'
+    'https://goldfish-app-hbefc.ondigitalocean.app/upload'
   return url
 }
 
