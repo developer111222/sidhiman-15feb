fc68aa172cada74a403ddb5a99c389a478b9ae88
developer111222 <anishprajapati765@gmail.com>
2025-02-24 14:07:29 +0530

    first commit

--- src/utils/GetSiteUrl.jsx
+++ src/utils/GetSiteUrl.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const GetSiteUrl = () => {
     const url =
-  'http://localhost:5000'
-    // 'https://jellyfish-app-irlgj.ondigitalocean.app'
+  // 'http://localhost:5000'
+    'https://goldfish-app-hbefc.ondigitalocean.app'
   return url
  
 }
--- src/utils/ImageUrl.jsx
+++ src/utils/ImageUrl.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const ImageUrl = () => {
     const url =
-  'http://localhost:5000/upload'
-    // 'https://jellyfish-app-irlgj.ondigitalocean.app/upload'
+  // 'http://localhost:5000/upload'
+    'https://goldfish-app-hbefc.ondigitalocean.app//upload'
   return url
 }
 
